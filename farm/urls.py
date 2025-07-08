7618c70fdba55ab151a24fb5df1c093cfddb8ecc
hildaAbigaba <abigabahilda@gmail.com>
2025-07-09 02:16:39 +0300

    Added a few adjustments on the manager app

--- farm/urls.py
+++ farm/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from manager.views import RecordsPage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('records/', RecordsPage)
 ]
--- manager/views.py
+++ manager/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def RecordsPage(request):
+    
+    return render(request,'records.html')
